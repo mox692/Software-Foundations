e82d08144d58ce47e7cc3aedb50634044ac1acd8
mox692 <55653825+mox692@users.noreply.github.com>
2022-12-10 16:58:15 +0900

    update

--- vol1/src/handson/Logic2.v
+++ vol1/src/handson/Logic2.v
@@ -43,3 +43,92 @@ Proof.
   Qed.
 
 
+(* Disjunction *)
+(* MEMO: conjunctionの時はsplitを使ったが、disjunctionの時は
+      destructを使う. *)
+Lemma factor_is_O: forall n m : nat,
+   n = 0 \/ m = 0 -> n * m = 0.
+Proof.
+  intros n m H1.
+  destruct H1.
+  * rewrite -> H.
+    reflexivity.
+  * rewrite -> H.
+    assert (n * 0 = 0) as H1.
+    {
+      induction n.
+      - reflexivity.
+      - simpl. 
+        apply IHn.
+    }
+    apply H1.
+  Qed.
+
+Lemma or_intro_l : forall A B : Prop,
+  A -> A \/ B.
+Proof.
+  intros A B H.
+  (* MEMO: leftをゴールに設定する. *)
+  left.
+  apply H.
+  Qed.
+
+Lemma zero_or_succ : forall n : nat,
+  n = 0 \/ n = S (pred n).
+Proof.
+  intros n.
+  destruct n.
+  * left.
+    reflexivity.
+  * right.
+    simpl.
+    reflexivity.
+  Qed.
+
+Theorem ex_falso_quodlibet : forall (P:Prop),
+  False -> P.
+Proof.
+  intros P H.
+  (* MEMO: 矛盾命題を分解するのにdestructしている *)
+  destruct H.
+  Qed.
+
+ (* MEMO: この証明の意味がよくわからん *)
+Theorem not_implies_our_not : forall (P:Prop),
+  not P -> (forall (Q:Prop), P -> Q).
+Proof.
+  intros P H0 Q H1.
+  destruct H0.
+  apply H1.
+  Qed.
+
+Notation "x <> y" := (~(x = y)).
+
+Theorem zero_not_one : 0 <> 1.
+Proof.
+  (* MEMO: x <> y -> not (x = y) -> (x = y -> false) ってことらしい *)
+  unfold not.
+  intros contra.
+  (* MEMO: discriminate についての復讐 ( https://softwarefoundations.cis.upenn.edu/lf-current/Tactics.html#:~:text=These%20examples%20are%20instances%20of%20a%20logical%20principle%20known%20as%20the%20principle%20of%20explosion%2C%20which%20asserts%20that%20a%20contradictory%20hypothesis%20entails%20anything%20(even%20manifestly%20false%20things!). ) *)
+  discriminate contra.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
